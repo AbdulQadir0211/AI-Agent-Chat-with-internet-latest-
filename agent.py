8ab057a1c639ace06356856348690b6e1be96653
AbdulQadir0211 <abdulkadir9929@gmail.com>
2025-03-06 08:39:01 +0530

    api updated

--- agent.py
+++ agent.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-GROQ_API_KEY = os.getenv("")
-TAVILY_API_KEY = os.getenv("")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
 
 # Setup LLM and tools
